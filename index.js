b106ab69dcb6ac72ec4a4ad62781f51962ef30da
Oliver Carlsson <oliver@framecore.se>
2021-01-02 00:41:49 +0100

    first commit

--- index.js
+++ index.js
@@ -3,22 +3,21 @@ const app = express();
 const { exec } = require('child_process');
 const server = require("http").createServer(app);
 const WebSocket = require("ws")
-const PORT = process.env.PORT || 3000;
 
 //to get directory path
 const path = require('path');
 const fs = require('fs');
 
 const wss = new WebSocket.Server({ server:server })
-
-
 app.use(express.static("public/"));
 
 
+let outputPath = `${__dirname}\\CLIPS`
+// Gives error/returns script if .exe file is not included in the outputPath
+let hasPathError = false;
 let clipsPath = 0;
 
 wss.on("connection", (ws) => {
-
   // ws.on("open", () => console.log("opened"));
   // ws.on("close", () => console.log("closed"))
   ws.on("message", (message) => {
@@ -28,53 +27,77 @@ wss.on("connection", (ws) => {
         downloadClip(result.clips[i].url)
       }
     }
+    if(result.method === "sendSettings") {
+      outputPath = result.path
+      clipsPath = 0;
+      makeFolder()
+      fs.readdir(`${outputPath}`, function (err, files) {
+        if (err) return console.log('Unable to scan directory: ' + err);
+        //listing all files using forEach
+        for(let file of files) {
+          if(file.includes(".exe")) {
+            hasPathError = false;
+          } else {
+            hasPathError = true;
+          }
+        }
+        sendPayLoad();
+      });
+
+      function sendPayLoad() {
+        const payLoad = {
+          method: "verifyPath",
+          verify: hasPathError
+        }
+        ws.send(JSON.stringify(payLoad))
+      }
+
+    }
   })
+  // Init client side content
+  const sendData = {
+    method: "givePath",
+    path: `${__dirname}/CLIPS`
+  }
+  ws.send(JSON.stringify(sendData))
 })
-
+ 
+// Download the actual clips
 function downloadClip(url) {
-  exec(`youtube-dl.exe -f best ${url}`, {cwd: `D:/CLIPS/${clipsPath}`}, (err, stdout, stderr) => {
-  if (err) {
-    // node couldn't execute the command
-    return;
-  }
-
+  exec(`youtube-dl.exe -f best ${url}`, {cwd: `${outputPath}/${clipsPath}`}, (err, stdout, stderr) => {
+  if (err) return;
   // the *entire* stdout and stderr (buffered)
   console.log(`stdout: ${stdout}`);
   console.log(`stderr: ${stderr}`);
   });
 }
 
-// Choose what folder name should be called
-fs.readdir("D:/CLIPS", function (err, files) {
-  //handling error
-  if (err) {
-      return console.log('Unable to scan directory: ' + err);
-  } 
-  //listing all files using forEach
-  files.forEach(function (file) {
-      // Do whatever you want to do with the file
-      console.log(file);
-      clipsPath++
-      console.log(clipsPath)
-  });
-  setPath()
-});
 
+function makeFolder() {
+  clipsPath = 0;
+  // Finds what the folder name should be called (in this case its integers that increments)
+  fs.readdir(`${outputPath}`, function (err, files) {
+    if (err) return console.log('Unable to scan directory: ' + err);
+
+    //listing all files using forEach
+    files.forEach(function (file) {
+        // console.log(file);
+        clipsPath++
+    });
+    setPath()
+  });
+}
+// Create a new folder for the clips
 function setPath() {
-  exec(`mkdir ${clipsPath} && copy youtube-dl.exe D:\\CLIPS\\${clipsPath}`, {cwd: `D:/CLIPS`}, (err, stdout, stderr) => {
-  if (err) {
-    // node couldn't execute the command
-    return;
-  }
-
+  exec(`mkdir ${clipsPath}`, {cwd: `${outputPath}`}, (err, stdout, stderr) => {
+  if (err) return;
   // the *entire* stdout and stderr (buffered)
-  console.log(`stdout: ${stdout}`);
-  console.log(`stderr: ${stderr}`);
+  console.log(`Standard Output: ${stdout}`);
+  console.log(`Standard Error: ${stderr}`);
   });
-}
+} 
 
 
-
-server.listen(PORT, () => {
+server.listen(3000, () => {
   console.log("LISTENING ON PORT 3000!");
 });
--- public/js/app.js
+++ public/js/app.js
@@ -1,21 +1,70 @@
 let HOST = location.origin.replace(/^http/, "ws");
 let ws = new WebSocket(HOST);
-console.log(HOST)
 
 ws.addEventListener("open", () => {
   console.log("We are connected!")
 });
 
-
 const button = document.querySelector("#btn")
+const inputPath = document.querySelector("#input-path")
+const clipsAmount = document.querySelector("#clips-amount")
+const clipsStarted = document.querySelector("#clips-started")
+const loading = document.querySelector(".loading")
+
+let setAmount = "";
+let clipsDate = null;
+
+// ######## CONFIGURATION ########
+
+// Create an application on https://dev.twitch.tv/console to get your client id and client secret
+const clientId = "your_client_id_here"
+const clientSecret = "your_client_secret_here" // note your client secret is private thus should not be shown to other people
+
+// ######## CONFIGURATION ########
+
 
 button.addEventListener("click", function() {
-  fetchClips();
+  setAmount = parseInt(clipsAmount.value)
+  switch(clipsStarted.value) {
+    case "1Year":
+      clipsDate = new Date(new Date().getTime() - (8760 * 60 * 60 * 1000)).toISOString();
+      break;
+    case "30Days":
+      clipsDate = new Date(new Date().getTime() - (720 * 60 * 60 * 1000)).toISOString();
+      break;
+    case "7Days":
+      clipsDate = new Date(new Date().getTime() - (168 * 60 * 60 * 1000)).toISOString();
+      break;
+    case "24Hours":
+      clipsDate = new Date(new Date().getTime() - (24 * 60 * 60 * 1000)).toISOString();
+      break;
+    default:
+      clipsDate = new Date(new Date().getTime() - (24 * 60 * 60 * 1000)).toISOString();
+  }
+
+    const payLoad = {
+      method: "sendSettings",
+      path: inputPath.value
+    }
+    ws.send(JSON.stringify(payLoad));
 });
 
+ws.onmessage = (message) => {
+  const response = JSON.parse(message.data);
+  if (response.method === "givePath") {
+    const path = response.path;
+    inputPath.value = path;
+  }
+  if (response.method === "verifyPath") {
+    const hasPathError = response.verify
+    if(hasPathError === false) fetchClips();
+  }
+}
 
-const userClips = [];
-const gameClips = [];
+
+
+let userClips = [];
+let gameClips = [];
 let userCounter = 0;
 let gameCounter = 0;
 let isUserClipsFetched = false;
@@ -23,9 +72,16 @@ let isGameClipsFetched = false;
 
 
 const fetchClips = async () => {
+  userClips = [];
+  gameClips = [];
+  userCounter = 0;
+  gameCounter = 0;
+  isUserClipsFetched = false;
+  isGameClipsFetched = false;
+  console.log(`FETCHING CLIPS...`)
   try {
     // First fetch access token so we can use the API
-    const res = await axios.post('https://id.twitch.tv/oauth2/token?client_id=str8k2ifmiexe3gqkd2ctq38pjhvoi&client_secret=sqhukdwucvdpjzdj997l3kq36y7whb&grant_type=client_credentials')
+    const res = await axios.post(`https://id.twitch.tv/oauth2/token?client_id=${clientId}&client_secret=${clientSecret}&grant_type=client_credentials`)
     const token = res.data.access_token;
     // Chain of multiple functions, in the end, returns big list of top categoriy/game clips
     fetchTopGames(token);
@@ -45,7 +101,7 @@ const fetchTopGames = async (token) => {
     const config = {
       headers:{
         "Authorization": `Bearer ${token}`,
-        "Client-Id": "str8k2ifmiexe3gqkd2ctq38pjhvoi"
+        "Client-Id": `${clientId}`
       }
     }
     const res = await axios.get('https://api.twitch.tv/helix/games/top?first=20', config)
@@ -60,25 +116,23 @@ const fetchTopGames = async (token) => {
 }
 
 const getAllGameClips = (token, gameIdList) => {
-  // Get 24 hours ago's date in RFC3339 format
-  const yesterday = new Date(new Date().getTime() - (24 * 60 * 60 * 1000)).toISOString();
   // Se we can excecute another function right after lastIndex is fetched
   const gameListLength = gameIdList.length;
   for(let i = 0; i < gameIdList.length; i++) {
     // Excecute this function for every gameId
-    fetchTopGameClipsOfTheDay(token, gameIdList[i], yesterday, gameListLength)
+    fetchTopGameClipsOfTheDay(token, gameIdList[i], gameListLength)
   }
 }
 
-const fetchTopGameClipsOfTheDay = async (token, gameId, date, gameListLength) => {
+const fetchTopGameClipsOfTheDay = async (token, gameId, gameListLength) => {
   try {
     const config = {
       headers:{
         "Authorization": `Bearer ${token}`,
-        "Client-Id": "str8k2ifmiexe3gqkd2ctq38pjhvoi"
+        "Client-Id": `${clientId}`
       }
     }
-    const res = await axios.get(`https://api.twitch.tv/helix/clips?game_id=${gameId}&started_at=${date}`, config)
+    const res = await axios.get(`https://api.twitch.tv/helix/clips?game_id=${gameId}&started_at=${clipsDate}`, config)
     for(let i = 0; i < 20; i++) {
       if(res.data.data[i] === undefined) continue;
       // Push top 5 clips and its views from a user
@@ -105,7 +159,7 @@ const fetchHandPickedUsers = async (token) => {
     const config = {
       headers:{
         "Authorization": `Bearer ${token}`,
-        "Client-Id": "str8k2ifmiexe3gqkd2ctq38pjhvoi"
+        "Client-Id": `${clientId}`
       }
     }
     const res = await axios.get('https://api.twitch.tv/helix/users?login=xQcOW&login=shroud&login=Myth&login=Pokimane&login=sodapoppin&login=summit1g&login=NICKMERCS&login=TimTheTatman&login=loltyler1&login=Symfuhny&login=Lirik&login=Anomaly&login=Asmongold&login=Mizkif&login=HasanAbi&login=ludwig&login=moistcr1tikal&login=MitchJones&login=Nmplol&login=JakenBakeLIVE&login=Knut&login=Maya&login=pokelawls&login=itssliker&login=EsfandTV&login=erobb221&login=drdisrespect', config)
@@ -120,25 +174,23 @@ const fetchHandPickedUsers = async (token) => {
 }
 
 const getAllUserClips = (token, userIdList) => {
-  // Get 24 hours ago's date in RFC3339 format
-  const yesterday = new Date(new Date().getTime() - (24 * 60 * 60 * 1000)).toISOString();
     // Se we can excecute another function right after lastIndex is fetched
     const userListLength = userIdList.length;
   for(let i = 0; i < userIdList.length; i++) {
     // Excecute this function for every userId
-    fetchTopClipsOfTheDay(token, userIdList[i], yesterday, userListLength)
+    fetchTopClipsOfTheDay(token, userIdList[i], userListLength)
   }
 }
 
-const fetchTopClipsOfTheDay = async (token, userId, date, userListLength) => {
+const fetchTopClipsOfTheDay = async (token, userId, userListLength) => {
   try {
     const config = {
       headers:{
         "Authorization": `Bearer ${token}`,
-        "Client-Id": "str8k2ifmiexe3gqkd2ctq38pjhvoi"
+        "Client-Id": `${clientId}`
       }
     }
-    const res = await axios.get(`https://api.twitch.tv/helix/clips?broadcaster_id=${userId}&started_at=${date}`, config)
+    const res = await axios.get(`https://api.twitch.tv/helix/clips?broadcaster_id=${userId}&started_at=${clipsDate}`, config)
     for(let i = 0; i < 10; i++) {
       if(res.data.data[i] === undefined) continue;
       // Push top 5 clips and its views from a user
@@ -160,7 +212,6 @@ const fetchTopClipsOfTheDay = async (token, userId, date, userListLength) => {
 
 
 const Combine = (gameClips, userClips) => {
-  console.log("time to BAKE")
 
   // Merges gameClips and userClips together
   const arr = [...gameClips, ...userClips]
@@ -174,14 +225,13 @@ const Combine = (gameClips, userClips) => {
   });
   // Sort clips from most viewed to least viewed
   const clipsSort = clipsNoDuplicate.sort((a, b) => (a.views < b.views) ? 1 : -1)
-  // Return the first 50 clips
-  const clipsReady = clipsSort.slice(0, 100);
-  console.log(clipsReady.slice(-1)[0])
+  // Return the first setAmount clips
+  const clipsReady = clipsSort.slice(0, setAmount);
   sendClips(clipsReady);
+  console.log(`DOWNLOADING CLIPS...`)
 }
 
 const sendClips = (clipsReady) => {
-  console.log(clipsReady)
   const payLoad = {
     method: "sendClips",
     clips: clipsReady
